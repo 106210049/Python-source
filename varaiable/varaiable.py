9a7c1e476de8669a31f39e2515b83e79fe70a75c
Hoàng Bảo Long <105693455+106210049@users.noreply.github.com>
2023-02-05 18:56:45 +0700

    Add files via upload

---
+++ varaiable/varaiable.py
@@ -0,0 +1,24 @@
+# interger
+x=15
+print("type of x: ",type(x))
+
+#float
+x=2.3
+print("type of x: ",type(x))
+
+# bool
+x=True
+print("type of x: ",type(x))
+
+# complex
+x=complex(23,45)
+print(x)
+print("type of x: ",type(x))
+
+#string
+x='hello'
+print("type of x: ",type(x))
+
+# test
+print(2.5+3)
+print(type(2.5+3))
